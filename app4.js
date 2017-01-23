c71fa93b4ccdfaaf364303b03ff77952e93e3a1b
Emmanuel Vidal <pruebasv02@hotmail.com>
2017-01-23 17:53:52 -0400

    Now Returns status 404 at /404 path.

--- app4.js
+++ app4.js
@@ -1,27 +1,13 @@
 /**
- * Create a program that outputs the HTTP method, Path, Port, and Header fields as a JSON object on the body of the Web server's response.
+ *  1. Return status code 404 at path /404
  */
 
 var express = require('express');
 var app = express();
 
-app.use(function (req, res, next) {
-
-    var jsonResult = new Object();
-
-    //http://stackoverflow.com/questions/6857468/converting-a-js-object-to-an-array
-    var headerArray = Object.keys(req.headers).map(function (key) { return req.headers[key]; });
-
-    jsonResult.header = headerArray;
-
-    jsonResult.method = req.method;
-    jsonResult.protocol = req.protocol;
-    jsonResult.path = req.path;
-    jsonResult.port = req.headers.host.split(':')[1];
-
-    res.send(jsonResult);
-
-    next();
+app.use('/404', function (req, res, next) {
+  res.status(404);  
+  res.send();
 });
 
 
