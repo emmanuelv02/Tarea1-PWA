52a82d6da18284e21fc9c6398ceaf370ab8e07a0
Emmanuel Vidal <pruebasv02@hotmail.com>
2017-01-23 22:35:33 -0400

    Limit to GET method.

--- app4.js
+++ app4.js
@@ -1,11 +1,11 @@
 /**
- *  1. Return status code 401 at path /protected
+ *  1. Return status code 401 at path /protected when get
  */
 
 var express = require('express');
 var app = express();
 
-app.use('/protected', function (req, res, next) {
+app.get('/protected', function (req, res) {
   res.status(401);  
   res.send();
 });
