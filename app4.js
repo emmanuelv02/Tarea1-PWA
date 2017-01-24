89a587f89e82ee04d97faf1ba9bebe21679ecabb
Emmanuel Vidal <pruebasv02@hotmail.com>
2017-01-23 22:36:25 -0400

    Limited to GET method.

--- app4.js
+++ app4.js
@@ -1,14 +1,13 @@
 /**
- *  1. Return status code 404 at path /404
+ *  1. Return status code 404 at path /404 when GET
  */
 
 var express = require('express');
 var app = express();
 
-app.use('/404', function (req, res, next) {
+app.get('/404', function (req, res) {
   res.status(404);  
   res.send();
 });
-
 
 app.listen(8083);
